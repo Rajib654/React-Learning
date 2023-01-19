749aae32d681114712448c742c7c07cdc1db79f4
Rajib654 <77357162+Rajib654@users.noreply.github.com>
2023-01-19 22:52:20 +0600

    change functional component into class component & learn its usecase

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+/*
+//React functional Component
 
 function App({ local }) {
   return (
@@ -9,9 +12,23 @@ function App({ local }) {
   );
 }
 
+*/
+
 //Here App function called component & only return section called element
 // One React Component return one single element
 // We can re-use React Component
 //Component received properties form outside & use this properties in their inside element.
+//Never Change props inside the compmenets
+// React component will re-rendered whenever props change
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1 className='heading'>
+        <span type="text">Hello-{this.props.children} {new Date().toLocaleTimeString(this.props.local)}</span>
+      </h1>
+    )
+  }
+}
 
 export default App;
